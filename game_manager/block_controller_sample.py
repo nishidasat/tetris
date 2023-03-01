ddcb491cfdbce20243d49f6fcb470904837b3277
nishidasat <124797608+nishidasat@users.noreply.github.com>
2023-03-01 19:53:06 +0900

    Update block_controller_sample.py

--- game_manager/block_controller_sample.py
+++ game_manager/block_controller_sample.py
@@ -260,9 +260,9 @@ class Block_Controller(object):
 
         if emergency == 0:
             if fullLines == 1:
-                score = score - fullLines * 10.0           # try to delete line 
+                score = score - fullLines * 20.0           # try to delete line 
             if fullLines == 2:
-                score = score - fullLines * 5.0           # try to delete line 
+                score = score - fullLines * 10.0           # try to delete line 
         
         score = score - nHoles * 10.0               # try not to make hole
         score = score - nIsolatedBlocks * 1.0      # try not to make isolated block
