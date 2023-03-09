7a62fefeb445f16fdce2472338aa91b9e85737f3
nishidasat <124797608+nishidasat@users.noreply.github.com>
2023-03-09 18:57:12 +0900

    Update block_controller_sample.py

--- game_manager/block_controller_sample.py
+++ game_manager/block_controller_sample.py
@@ -75,6 +75,9 @@ class Block_Controller(object):
                 if EvalValue > LatestEvalValue:
                     strategy = (direction0, x0, 1, 1)
                     LatestEvalValue = EvalValue
+                elif (EvalValue == LatestEvalValue) and (x0 % 2 == 0): # only case of even number
+                    strategy = (direction0, x0, 1, 1)
+                    LatestEvalValue = EvalValue
                 print('direction0=' + str(direction0) + '　x0=' + str(x0))
                     
                 ###test
