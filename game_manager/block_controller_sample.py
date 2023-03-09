e94d8d48cee883ea42ab235486604bfa8b00954e
nishidasat <124797608+nishidasat@users.noreply.github.com>
2023-03-10 01:53:08 +0900

    Update block_controller_sample.py

--- game_manager/block_controller_sample.py
+++ game_manager/block_controller_sample.py
@@ -280,8 +280,8 @@ class Block_Controller(object):
         score = score - nHoles * 10.0               # try not to make hole
         score = score - nIsolatedBlocks * 1.5      # try not to make isolated block
         score = score - absDy * 1.0                # try to put block smoothly
-        score = score - maxHeight * 0.1              # maxHeight
-        score = score + xxdy * 0.1              # minHeight
+        score = score - maxHeight * 0.01              # maxHeight
+        score = score + xxdy * 0.01              # minHeight
         
         #********************************************************************************
         #score = score - maxDy * 0.3                # maxDy
