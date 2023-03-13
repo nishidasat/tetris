56948c4d5c61204d4e1017e6babecb0b529bdf13
nishidasat <124797608+nishidasat@users.noreply.github.com>
2023-03-13 23:36:36 +0900

    Update block_controller_sample.py

--- game_manager/block_controller_sample.py
+++ game_manager/block_controller_sample.py
@@ -268,8 +268,8 @@ class Block_Controller(object):
         elif (CurrentShape_index >= 4) and (emergency == 0):
             score = score + fullLines * 10.0           # try to delete line
             score = score - absDy * 1.0                # try to put block smoothly
-            #if fullLines == 1:
-            #    score = score - fullLines * 1.0           # try to delete line 
+            if fullLines == 1:
+                score = score - fullLines * 1.0           # try to delete line 
         else:   # IN EMERGENCY
             score = score + fullLines * 10.0           # try to delete line
             #score = score - absDy * 1.0                # try to put block smoothly
