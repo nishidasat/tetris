e07f4159877861265fb304bed094251d672b3dc2
nishidasat <124797608+nishidasat@users.noreply.github.com>
2023-03-25 15:04:13 +0900

    Update block_controller_sample.py

--- game_manager/block_controller_sample.py
+++ game_manager/block_controller_sample.py
@@ -328,7 +328,7 @@ class Block_Controller(object):
             score = score + fullLines * 10.0           # try to delete line
             score = score - nHoles * 10.0               # try not to make hole
             score = score - nIsolatedBlocks * 1.5      # try not to make isolated bloc
-            #score = score - absDy * 0.01                # try to put block smoothly
+            score = score - absDy * 1.5                # try to put block smoothly
             #score = score - maxHeight * 0.01              # maxHeigh
             score = score - xxdy * 0.01             # block_minHeight 0.01
             #if fullLines == 1:
@@ -338,7 +338,7 @@ class Block_Controller(object):
             #elif fullLines == 4:
             #    score = score + fullLines * 1000.0           # try to delete line
             if xxdy <= 12:
-                score = score + (22 - xxdy) * 5.0           # try to delete line
+                score = score + (22 - xxdy) * 0.1           # try to delete line
             #if xxdy >= 19:
             #    score = score - xxdy * 1000.0           # try to delete line
                 
