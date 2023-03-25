6c3a403ae49c85203e9b1d2dd72671bc08f0776c
nishidasat <124797608+nishidasat@users.noreply.github.com>
2023-03-25 21:31:11 +0900

    Update block_controller_sample.py

--- game_manager/block_controller_sample.py
+++ game_manager/block_controller_sample.py
@@ -340,7 +340,7 @@ class Block_Controller(object):
             #elif fullLines == 4:
             #    score = score + fullLines * 1000.0           # try to delete line
             if xxdy <= 15:
-                score = score + (22 - xxdy) * 5.0           # try to delete line
+                score = score + (22 - xxdy) * 10.0           # try to delete line
             #if xxdy >= 19:
             #    score = score - xxdy * 1000.0           # try to delete line
                 
