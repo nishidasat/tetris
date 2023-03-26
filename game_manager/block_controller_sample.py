dad6548014c662918836b4fd649f0a71b8e9275a
nishidasat <124797608+nishidasat@users.noreply.github.com>
2023-03-26 13:44:49 +0900

    Update block_controller_sample.py

--- game_manager/block_controller_sample.py
+++ game_manager/block_controller_sample.py
@@ -281,10 +281,8 @@ class Block_Controller(object):
                 score = score - fullLines * 30.0           # try to delete line 
             elif (nHoles <= 5) and (fullLines == 2):
                 score = score - fullLines * 30.0           # try to delete line 1000
-            elif (nHoles >= 6) and (fullLines == 3):
+            elif (nHoles >= 6) and (fullLines == 2):
                 score = score + fullLines * 10.0           # try to delete line 1000
-            elif fullLines == 2:
-                score = score - fullLines * 30.0           # try to delete line
             elif fullLines == 3:
                 score = score + fullLines * 1000.0           # try to delete line 
             elif fullLines == 4:
@@ -303,9 +301,9 @@ class Block_Controller(object):
                 score = score - fullLines * 30.0           # try to delete line 
             elif fullLines == 2:
                 score = score - fullLines * 20.0           # try to delete line
-            elif (nHoles <= 5) and (fullLines == 3):
-                score = score - fullLines * 10.0           # try to delete line 1000
-            elif (nHoles >= 6) and (fullLines == 3):
+            elif (nHoles <= 6) and (fullLines == 3):
+                score = score - fullLines * 20.0           # try to delete line 1000
+            elif (nHoles >= 7) and (fullLines == 3):
                 score = score + fullLines * 10.0           # try to delete line 1000
                      
         #elif (CurrentShape_index >= 4) and (emergency == 0) and (unsafe == 1): # In safe
@@ -329,8 +327,8 @@ class Block_Controller(object):
             score = score - xxdy * 0.01                # block_minHeight 0.05
             if fullLines == 1:
                 score = score - fullLines * 5.0           # try to delete line
-            elif (nHoles <= 5) and fullLines == 2:
-                score = score - fullLines * 2.0           # try to delete line
+            elif (nHoles <= 6) and fullLines == 2:
+                score = score - fullLines * 3.0           # try to delete line 2
                 
         else:   # IN EMERGENCY
             score = score + fullLines * 13.0           # try to delete line ******
