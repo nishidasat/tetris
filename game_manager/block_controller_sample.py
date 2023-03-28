6ce36719905ac671f0bcdc205b4b005dc73f9477
nishidasat <124797608+nishidasat@users.noreply.github.com>
2023-03-29 01:22:02 +0900

    Update block_controller_sample.py

--- game_manager/block_controller_sample.py
+++ game_manager/block_controller_sample.py
@@ -269,11 +269,11 @@ class Block_Controller(object):
             score = score - absDy * 1.5                # try to put block smoothly
             #score = score - maxHeight * 0.01              # maxHeigh
             score = score - xxdy * 0.01                # block_minHeight 0.01
-            if (fullLines == 1) and (maxHeight <= 10):
+            if (fullLines == 1) and (maxHeight <= 8):
                 score = score - fullLines * 30.0           # try to delete line 
-            elif (fullLines == 2) and  (maxHeight <= 10):
+            elif (fullLines == 2) and  (maxHeight <= 8):
                 score = score - fullLines * 30.0           # try to delete line 1000
-            elif (fullLines == 2) and (maxHeight >= 11):
+            elif (fullLines == 2) and (maxHeight >= 9):
                 score = score + fullLines * 10.0           # try to delete line 1000
             elif fullLines == 3:
                 score = score + fullLines * 1000.0           # try to delete line 
@@ -289,13 +289,13 @@ class Block_Controller(object):
             score = score - absDy * 1.5                # try to put block smoothly
             #score = score - maxHeight * 0.01              # maxHeigh
             score = score - xxdy * 0.01                # block_minHeight 0.01
-            if (fullLines == 1) and (maxHeight <= 11):
+            if (fullLines == 1) and (maxHeight <= 8):
                 score = score - fullLines * 30.0           # try to delete line 
-            elif (fullLines == 2) and (maxHeight <= 11):
+            elif (fullLines == 2) and (maxHeight <= 8):
                 score = score - fullLines * 20.0           # try to delete line
-            elif (fullLines == 3) and (maxHeight <= 9):
+            elif (fullLines == 3) and (maxHeight <= 8):
                 score = score - fullLines * 5.0           # try to delete line 1000
-            elif (nHoles >= 5)  and (maxHeight >= 10):
+            elif (nHoles >= 5)  and (maxHeight >= 9):
                 score = score + fullLines * 30.0           # try to delete line 1000
                      
         elif (CurrentShape_index >= 4) and (emergency == 0):   #4~ NOT IN Emergency
