6d37ff28deabac8c841fe7cc3d225ee53526d7bd
nishidasat <124797608+nishidasat@users.noreply.github.com>
2023-03-29 14:29:57 +0900

    Update block_controller_sample.py

--- game_manager/block_controller_sample.py
+++ game_manager/block_controller_sample.py
@@ -316,10 +316,10 @@ class Block_Controller(object):
                 
         else:   # ALL Shape IN EMERGENCY 
             score = score + fullLines * 10.0           # try to delete line ******
-            score = score - nHoles * 10.0               # try not to make hole
-            score = score - nIsolatedBlocks * 1.5      # try not to make isolated bloc
+            #score = score - nHoles * 10.0               # try not to make hole
+            #score = score - nIsolatedBlocks * 1.5      # try not to make isolated bloc
             score = score - absDy * 1.5                # try to put block smoothly
-            #score = score - maxHeight * 0.01              # maxHeigh
+            score = score - maxHeight * 0.01              # maxHeigh
             score = score - xxdy * 0.05                  # block_minHeight 0.01
             if fullLines == 2:
                 score = score + fullLines * 100.0           # try to delete line
