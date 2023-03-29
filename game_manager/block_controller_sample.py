72b4c1fb915cecf7899765f4a67f7b3e4035d5e2
nishidasat <124797608+nishidasat@users.noreply.github.com>
2023-03-29 23:28:12 +0900

    Update block_controller_sample.py

--- game_manager/block_controller_sample.py
+++ game_manager/block_controller_sample.py
@@ -302,7 +302,7 @@ class Block_Controller(object):
             #score = score - maxHeight * 0.01              # maxHeigh
             score = score - xxdy * 0.01                # block_minHeight 0.05
             if (fullLines == 1) and (maxHeight <= 4):
-                score = score - fullLines * 10.0           # try to delete line 5
+                score = score - fullLines * 9.0           # try to delete line 5
             #if (fullLines == 1) and ((nHoles <= 3) and (maxHeight <= 4)):
             #    score = score - fullLines * 5.0           # try to delete line 5
             #elif (fullLines == 1):
